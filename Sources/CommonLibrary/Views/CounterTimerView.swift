6299818f1d8c2a15bdd117da04f87a417584de5a
DavidMurphy1088 <119924755+DavidMurphy1088@users.noreply.github.com>
2023-12-07 01:00:09 +1300

    scales

--- Sources/CommonLibrary/Views/CounterTimerView.swift
+++ Sources/CommonLibrary/Views/CounterTimerView.swift
@@ -2,13 +2,23 @@ import SwiftUI
 import Combine
 
 public struct CountdownTimerView: View {
-    let score:Score
-    @State private var timeRemaining = 30
+    let size:Double
+    let timerColor:Color
+    let startNotification: (() -> Void)?
+    let endNotification: (() -> Void)?
+    var timeLimit:Binding<Double>
+    
+    @State var timeRemaining = 0.0
     @State private var timer: AnyCancellable?
     @State private var isActive = false
     
-    public init(score:Score) {
-        self.score = score
+    public init(size:Double, timerColor:Color, timeLimit:Binding<Double>,
+                startNotification: (() -> Void)?, endNotification: (() -> Void)?) {
+        self.size = size
+        self.timerColor = timerColor
+        self.timeLimit = timeLimit
+        self.startNotification = startNotification
+        self.endNotification = endNotification
     }
     
     public var body: some View {
@@ -19,8 +29,13 @@ public struct CountdownTimerView: View {
                     self.timer = nil
                 } else {
                     if timeRemaining == 0 {
-                        timeRemaining = 30
+                        //timeRemaining = 30
                     }
+                    timeRemaining = timeLimit.wrappedValue
+                    if let startNotification = startNotification {
+                        startNotification()
+                    }
+
                     self.timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
                         .sink { _ in
                             if self.timeRemaining > 0 {
@@ -28,51 +43,50 @@ public struct CountdownTimerView: View {
                             } else {
                                 self.timer?.cancel()
                                 self.isActive = false
+                                if let endNotification = endNotification {
+                                    endNotification()
+                                }
                             }
                         }
                 }
                 self.isActive.toggle()
             }) {
-                Text(isActive ? "Pause Timer" : "Start Timer")
+                Text(isActive ? "Stop Timer" : "Start Timer")
             }
             .padding()
             .background(Color.blue)
             .foregroundColor(.white)
             .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            CircularProgressView(progress: CGFloat(timeRemaining) / 30.0,
-                                 timeRemaining: timeRemaining)
-                .frame(width: score.lineSpacing * 3, height: score.lineSpacing * 3)
+            CircularProgressView(progress: timeRemaining,
+                                 timeRemaining: Int(timeRemaining),
+                                 color: timerColor)
+                .frame(width: size, height: size)
                 .padding(20)
         }
     }
 }
 
-public struct CircularProgressView: View {
+struct CircularProgressView: View {
     var progress: CGFloat
     var timeRemaining: Int
+    var color:Color
     
-    public init(progress: CGFloat, timeRemaining: Int) {
-        self.progress = progress
-        self.timeRemaining = timeRemaining
-    }
-    
-    public var body: some View {
+    var body: some View {
         ZStack {
             Circle()
                 .stroke(lineWidth: 5)
                 .opacity(timeRemaining == 0 ? 1.0 : 0.3)
-                .foregroundColor(timeRemaining == 0 ? Color.red : Color.blue)
-
+                .foregroundColor(timeRemaining == 0 ? Color.red : color)
+            
             Circle()
                 .trim(from: 0, to: progress)
-                .stroke(timeRemaining == 0 ? Color.red : Color.blue, style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .stroke(timeRemaining == 0 ? Color.red : color, style: StrokeStyle(lineWidth: 5, lineCap: .round))
                 .rotationEffect(.degrees(-90))
                 .aspectRatio(contentMode: .fit)
-
-            Text("\(timeRemaining)").foregroundColor(timeRemaining == 0 ? Color.red : Color.black)
-                //.font(.footnote)
+            
+            Text("\(timeRemaining)")
+            //.font(.footnote)
         }
     }
 }
-
